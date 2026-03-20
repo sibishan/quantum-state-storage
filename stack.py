38360ec570692d33e59eb77e25e910e6e998783c
Michael Minaca <michaelsminaca@gmail.com>
2026-03-20 14:40:35 +1100

    removed peak method from stack class

--- stack.py
+++ stack.py
@@ -20,21 +20,8 @@ class Stack:
         self.top -= 1
         return self.items.pop()
 
-    def peek(self):
-        if self.isEmpty():
-            return None
-        return self.items[-1]
-
     def isEmpty(self):
         return len(self.items) == 0
 
     def size(self):
         return len(self.items)
-
-my_stack = Stack(items=[1,2,3,4,5,6,7,8,9], capacity=10)
-my_stack.push(5)
-my_stack.push(6)
-my_stack.push(7)
-print(my_stack.pop())
-print(my_stack.peek())
-my_stack.size()
