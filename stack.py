6fc82aaec491e5bde8dc1ab02a5890c1f7066a0d
Sibishan Ravindran <sibishanr@gmail.com>
2026-03-21 17:49:32 +1100

    stack utils added

--- stack.py
+++ stack.py
@@ -4,12 +4,9 @@ class QStack:
     def __init__(self, num_qubits=0, num_clones=0):
         self.num_qubits = num_qubits
         self.num_clones = num_clones
-        self.capacity = 0
+        self.count = 0
         self.protocol = Protocol(self.num_qubits, self.num_clones)
         self._get_qc = False
-    
-    def draw(self):
-        return self.protocol.qc.draw(output='mpl', fold=-1)
 
     def push(self, qc=None):
         if self._get_qc:
@@ -18,29 +15,40 @@ class QStack:
             raise ValueError("Input circuit cannot be None")
         if qc.num_qubits != 1:
             raise ValueError("Input must be a single-qubit circuit")
-        if self.capacity >= (self.num_qubits):
+        if self.count >= (self.num_qubits):
             raise OverflowError("QStack Overflow")
         
-        self.protocol.store_qubit(qc, self.capacity)
-        self.capacity += 1
+        self.protocol.store_qubit(qc, self.count)
+        self.count += 1
 
     def pop(self, c_index=0):
         if self._get_qc:
             raise RuntimeError("Cannot pop qubits after finalising the protocol circuit")
-        if self.isEmpty():
+        if self.is_empty():
             raise IndexError("QStack is empty")
         if c_index >= self.num_clones or c_index < 0:
             raise ValueError("Invalid clone index")
         
-        temp = self.capacity - 1
+        temp = self.count - 1
         self.protocol.retrieve_qubit(temp)
-
-    def isEmpty(self):
-        return self.capacity == 0
+    
+    def draw(self):
+        return self.protocol.qc.draw(output='mpl', fold=-1)
     
     def generate_circuit(self):
         self._get_qc = True
         return self.protocol.get_qc()
 
+    def is_empty(self):
+        return self.count == 0
+    
+    def is_full(self):
+        return self.count == self.num_qubits
+    
     def size(self):
-        return len(self.capacity)
+        return self.count
+    
+    def clear(self):
+        self.count = 0
+        self.protocol = Protocol(self.num_qubits, self.num_clones)
+        self._get_qc = False
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from qiskit.quantum_info import (
     partial_trace, state_fidelity
 )
 
-A = 3
+A = 2
 S = 3
 
 stack = QStack(num_qubits=A, num_clones=S)
@@ -15,14 +15,14 @@ a = QuantumCircuit(1)
 psi = random_statevector(2)
 a.initialize(psi, 0)
 
+print(stack.size())
 stack.push(a)
+print(stack.size())
 stack.pop()
+print(stack.size())
 # qc = stack.generate_circuit()
 stack.push(a)
-stack.pop()
-
-stack.push(a)
-stack.pop()
+print(stack.size())
 
 img = stack.draw()
 img.savefig('misc/circuit.png', bbox_inches='tight')
